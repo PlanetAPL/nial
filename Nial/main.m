947d27803e23670dfc1849ef1c880f48b70f08f6
Robert M. Lefkowitz <r0ml@me.com>
2013-02-09 23:28:53 -0500

    Initial Commit

---
+++ Nial/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Nial
+//
+//  Created by Robert M. Lefkowitz on 2/9/13.
+//  Copyright (c) 2013 Robert M. Lefkowitz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
